110e78055ae9149816d23231d035223724a01fa4
JeongJihun <demd4784@gmail.com>
2022-11-20 17:50:33 +0900

    Add files via upload

--- JAVA/thisisjava/src/Ch17/sec06/exam02/MapExample.java
+++ JAVA/thisisjava/src/Ch17/sec06/exam02/MapExample.java
@@ -9,7 +9,7 @@ public class MapExample {
 
     public static void main(String[] args) {
         int[] intArray = {1,2,3,4,5};
-        int[] intArr = {6,7,8,9};
+        
         Arrays.stream(intArray).asDoubleStream().forEach(d -> System.out.println(d)); // mapToDouble?
         System.out.println();
 
